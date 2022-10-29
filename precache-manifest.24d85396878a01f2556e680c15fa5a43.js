a8d699ccb24febb719950d793612b28a450f5b21
Arbaazezio <arbaazvsezio@hotmail.com>
2022-10-29 17:57:09 +0400

    Updates

---
+++ precache-manifest.24d85396878a01f2556e680c15fa5a43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ebde568512260392fe4",
+    "url": "/Arbaazezio/robotfriend.git/static/css/main.5025258f.chunk.css"
+  },
+  {
+    "revision": "5ebde568512260392fe4",
+    "url": "/Arbaazezio/robotfriend.git/static/js/main.5ebde568.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/Arbaazezio/robotfriend.git/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "3e4c00e9c8c6e935031a",
+    "url": "/Arbaazezio/robotfriend.git/static/css/2.cabe27f3.chunk.css"
+  },
+  {
+    "revision": "3e4c00e9c8c6e935031a",
+    "url": "/Arbaazezio/robotfriend.git/static/js/2.3e4c00e9.chunk.js"
+  },
+  {
+    "revision": "779de5d9b7eab48db090",
+    "url": "/Arbaazezio/robotfriend.git/static/js/runtime~main.779de5d9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Arbaazezio/robotfriend.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ce22df82d354f625802135a427eda200",
+    "url": "/Arbaazezio/robotfriend.git/index.html"
+  }
+];
